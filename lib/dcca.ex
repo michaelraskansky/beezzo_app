272132d55e26f61f75197632deebfef88e57da66
Michael Raskasnky <michaelraskansky@gmail.com>
2014-02-16 18:04:50 +0200

    Working

--- lib/dcca.ex
+++ lib/dcca.ex
@@ -2,6 +2,7 @@ defmodule Dcca do
   use Application.Behaviour
 
   def start(_type, _args) do
+    IO.puts "#{__MODULE__}.start"
     Dcca.Supervisor.start_link([])
   end
 
--- lib/dcca/supervisor.ex
+++ lib/dcca/supervisor.ex
@@ -2,12 +2,15 @@ defmodule Dcca.Supervisor do
   use Supervisor.Behaviour
 
   def start_link(opts) do
+    IO.puts "#{__MODULE__}.start_link"
     :supervisor.start_link(__MODULE__, [opts])
   end
 
   def init(opts) do
+    IO.puts "#{__MODULE__}.init"
     children = [
-      worker(Dcca.Stack, [opts])
+      worker(Dcca.Peer.Supervisor, [opts]),
+      worker(Dcca.Stack.Main, [opts])
     ]
 
     supervise(children, strategy: :one_for_one)
